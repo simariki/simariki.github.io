a7468a9462f3dd57fbd4398b7be4c3b62659a16e
simariki <m0a2313796@edu.teu.ac.jp>
2024-11-27 18:01:41 +0900

    11/20

--- 241120/animation.js
+++ 241120/animation.js
@@ -1,6 +1,19 @@
 function startAnimation() {
     anime({
-        targets: "#animation-box",
-        translateX:400,
-    })
+        targets: "#start-button",
+        rotate:'360deg',　　//回転する
+        easing:'linear',
+    });
+
+    anime({
+        targets:"#animation-box",
+        keyframes:[ //上から順番に行ってゆく　loopを使う際に使う
+            {opacity:0},
+            {opacity:1}
+            ],
+        easing:'linear',
+        duration:'200',
+        loop:true
+    });
 }
+
--- 241120/sheet.js
+++ 241120/sheet.js
@@ -1,16 +1,17 @@
-let url="https://sheets.googleapis.com/v4/spreadsheets/1khftIOwrp4_1qrBF1vWg4I9LjKGKFAk1gdfc_SVgWcU/values/sheet1?key=AIzaSyBWN4pynC1PzYGwVMHLYh84w0KjAzAWmYY";
-    fetch(url).then(
-        function (response) {
-            return response.json();
-        }
+let url = "https://sheets.googleapis.com/v4/spreadsheets/1khftIOwrp4_1qrBF1vWg4I9LjKGKFAk1gdfc_SVgWcU/values/sheet1?key=AIzaSyBWN4pynC1PzYGwVMHLYh84w0KjAzAWmYY";
+fetch(url).then(
+    function (response) {
+        return response.json();
+    }
+    //fetchで、指定したものの情報を選別。.thenで情報を読み取る　returnでとってきた情報をこのサイトで読み取る
 ).then(
-    (json)=>{
-        let answerElement=document.querySelector('#answer');
-        let valueHtml ='';
-        json.values.forEach((row)=>{
+    (json) => {
+        let answerElement = document.querySelector('#answer');
+        let valueHtml = '';
+        json.values.forEach((row) => {
             valueHtml += `<li>${row[0]}: ${row[1]} ${row[2]}</li>`
         });
-        answerElement.innerHTML= `<ul>${valueHtml}</ul>`;
+        answerElement.innerHTML = `<ul>${valueHtml}</ul>`;
 
     }
 );
